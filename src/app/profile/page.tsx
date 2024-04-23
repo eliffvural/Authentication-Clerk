277e1ed4c06b3dcfbbd45d3a26413efe9ae3f395
Elif Vural <elifvural019@gmail.com>
2024-04-23 13:01:48 +0300

    login and sign up parts completed

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { UserProfile } from "@clerk/nextjs";
+
+
+const page=()=>{
+    return (
+        <div className="flex items-center justify-center" >
+            <UserProfile/>
+        </div>
+    )
+};
+ 
+
+export default page;
